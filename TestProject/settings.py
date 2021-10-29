50cc134868a7920c46ce70540bf2557ab4dfc274
James Francis Omay <jfomay@addu.edu.ph>
2021-10-30 06:42:49 +0800

    sda

--- TestProject/settings.py
+++ TestProject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = ['jfomay-full-stack.herokuapp.com', '198.211.99.20', 'localhost'
 
 
 # Application definition
-
+django_heroku.settings(locals())
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
